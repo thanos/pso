dfdfe653d7305cea5a9a9aab231a3c23dd174247
thanos <thanosv@gmail.com>
2018-09-01 00:00:00 -0400

    added code

---
+++ py/pso/__init__.py
@@ -0,0 +1,10 @@
+#
+#   __init__.py  - Python Service Objects
+#
+#   Author: Thanos Vassilakis thanos@0x01.com
+#   licience: GPL
+#   (c) thanos vassilakis 2000,2001, 2002
+#
+#   $Id: __init__.py,v 1.1.1.1 2002/04/03 16:55:00 thanos Exp $
+#
+__version__="$Revision: 1.1.1.1 $"
