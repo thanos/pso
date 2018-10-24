64d722d83f14cd488c41bf9091dc8ab91aa61367
thanos <thanosv@gmail.com>
2018-10-24 00:00:00 -0400

    adding setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import sys
+
+from distutils.core import setup
+
+setup(name="pso",
+		version="0.98.6-beta",
+		description="Python Service Objects",
+		author="thanos vassilakis",
+		author_email="thanos@scriptfoundry.com",
+		url="http://pso.sourceforge.net/",
+		package_dir = {'': 'py'},
+		packages=['pso'],
+           )
